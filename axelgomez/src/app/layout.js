3ed91daae27194ba7018d3f086d1f6b4fe8ce54d
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-04-23 11:07:42 -0300

    cambio final

--- axelgomez/src/app/layout.js
+++ axelgomez/src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Full Stack Developer - Axel Gomez",
-  description: "Potencia tu búsqueda de empleo con los mejores consejos en tecnología, desde tips y plantillas de CV hasta preguntas típicas de entrevistas.",
+  description: "Hola! soy axel Gomez desarrollador Full Stack React / node",
   keywords: ["Axel Gomez", "developer", "full stack", "backend"],
   icons: {
     icon: ["/favicon.ico"]
@@ -16,7 +16,7 @@ export const metadata = {
     title: "Full Stack Developer - Axel Gomez",
     description: "Hola! soy axel Gomez desarrollador Full Stack React / node",
     image: "/favicon.ico",
-    url: "https://desarrollo-laboral.vercel.app"
+    url: "https://axelgomez.vercel.app/"
   }
 };
 
